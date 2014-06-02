a35fa07637e4095fc7d930c33809fc79efac991c
Bobby Eickhoff <bobby.eickhoff2@kroger.com>
2014-06-02 14:42:26 -0400

    An hour-and-a-half later

--- src/dojo/week32.clj
+++ src/dojo/week32.clj
@@ -1,7 +1,22 @@
 (ns dojo.week32
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn read-file-into-seq []
+  (with-open [rdr (clojure.java.io/reader
+                    "/Users/be65039/Development/open/cincy-code-dojo/dojo.week32/resources/words.txt")]
+    (doall (line-seq rdr))))
+
+(defn length-of-word [word]
+  (.length word)
+)
+
+(defn getwords [word-length]
+  (filter #(= (length-of-word %) word-length)
+    (read-file-into-seq)))
+
+(defn get-possible-passwords [length words]
+  (take length words))
+
+(defn compare-words [word1 word2]
+  (count (filter true? (map = word1 word2)))
+  )
--- test/dojo/week32_test.clj
+++ test/dojo/week32_test.clj
@@ -2,5 +2,19 @@
   (:require [midje.sweet :refer :all]
             [dojo.week32 :refer :all]))
 
-(fact "FIXME, I fail."
-  0 => 1)
+(fact "can read file"
+  (count (read-file-into-seq)) => 172820)
+
+(fact "can get list of 7 letter words"
+  (count  (getwords 7)) => 23109)
+
+(fact "get length of word"
+  (length-of-word "prateek") => 7)
+
+(fact "can get specified number of random words"
+  (count (get-possible-passwords 10 (getwords 7))) => 10)
+
+(fact "can return how many letters match guess"
+  (compare-words "prateek" "prateek") => 7
+  (compare-words "mind" "mend") => 3
+  (compare-words "compute" "playful") => 0)
